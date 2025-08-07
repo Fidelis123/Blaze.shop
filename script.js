47e27deab85ac86b8a1ad41bd696df55f687672d
Fidelis Udenwa <fiojinkama2020@gmail.com>
2025-08-06 21:05:37 -0700

    Contact

--- script.js
+++ script.js
@@ -54,3 +54,212 @@ const headContent = `
                 link.classList.remove('active');
             }
     });
+
+    // dynamic products// 
+    const featured = document.getElementById("featured");
+
+    function renderfeatured(locationLink, imgsrc, size, name, price) {
+     
+        const featuredHtml = `
+        <div class="feat-box" onclick="${locationLink}">
+                <img src="${imgsrc}" alt="${name}">
+                <div class="imgunder">
+                    <span>${size}</span>
+                    <h5>${name}</h5>
+                    <div class="star">
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <i class="bi bi-star"></i>
+                    </div>
+                        <p>${price}</p>
+                </div>
+                <a href="#"><i class="fas fa-shopping-cart cart"></i></a>
+            </div>
+        `;
+        featured.insertAdjacentHTML("beforeend", featuredHtml);
+    }
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "Medium size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "Large size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+        
+
+        renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+         renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+         renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+         renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+         renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+
+         renderfeatured(
+            "window.location.href='sfeatures.html';",
+            "./images/d7136c6c-65c3-4f44-aaf1-c13e2ec9beb9.jpg",
+            "small size",
+            "meat pie",
+            "₦20,000"
+        );
+
+        // Dynamics newsletter
+    
+
+    const newsLetter = document.getElementById("newsLetter"); 
+    const newsText =`
+        <div class="newstext">
+            <h4>sign up for newletters</h4>
+            <p>Get E-mail updates about our latest shop and <span>special offers.</span></p>
+        </div>
+            <form action="" class="form flexCenter">
+                <input type="text" name="" placeholder="your email address" required>
+                <button class="" required="">sign up</button>
+            </form>
+        `;
+    newsLetter.insertAdjacentHTML("beforeend", newsText);
+
+
+    //  Dynamics footer tag
+    const footer = document.getElementById("footer");
+    const footerHtml = `
+    <div class="col">
+                <img class="logo" src="images/4e383d5c-7eb5-4276-b635-c5e500225c24.jpg" alt=""
+                style="position: absolute; bottom: -1px;border-radius: 50%;border: #ca9c9f solid;">
+                <h4 style="text-decoration: underline;">Contact</h4>
+
+                <p> <strong>Address:</strong>no.3b hhatchery road Nkaliki</p>
+                <p> <strong>phone:</strong> +2348103628432</p>
+                <p> <strong>hours:</strong>10:00 - 18:00, Mon - Sat</p>
+                <div class="follow">
+                    <h4>follow us</h4>
+                    <div class="icon">
+                        <i class="bi bi-whatsapp"></i>
+                        <i class="bi bi-instagram"></i>
+                        <i class="bi bi-twitter-x"></i>
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-pinterest"></i>
+                        <i class="bi bi-youtube"></i>
+                    </div>
+                </div> 
+            </div>
+
+            <div class="col">
+                <h4>About</h4>
+                <a href="">About us</a>
+                <a href="">Delivery information</a>
+                <a href="">Privacy Policy</a>
+                <a href="">Terms & Condition</a>
+                <a href="">Contact us</a>
+            </div>
+
+            <div class="col">
+                <h4>My account</h4>
+                <a href="">Sign In </a>
+                <a href="">View cart</a>
+                <a href="">My wishlist</a>
+                <a href="">Track my order</a>
+                <a href="">Help</a>
+            </div>
+
+            <div class="col">
+                <h4>Install App</h4>
+                <p>From App Store or Google play</p>
+                <div class="row">
+                    <img src="./c63de450df4c84bc4f7d1b0a762d8d56.jpg" alt="">
+                    <img src="./f37da494b60e3b7ed6e3e08a1ddba2fa.jpg" alt="">
+                </div>
+                <p>Secure payment gateway</p>             
+            </div>
+    </div>
+
+    <div class="copy-right">
+            <p>© 2025, the shopping app</p>
+        </div>
+    `;
+    footer.insertAdjacentHTML("beforeend", footerHtml);
